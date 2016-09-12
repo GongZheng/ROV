33049c4f0732b99172b3ff82c93affee7603c1bf
LiL <Li@LiLdeMBP.lan>
2016-09-12 15:42:59 +0800

    更新初始化脚本

--- raspberry pi/script/startup/ras_script.sh
+++ raspberry pi/script/startup/ras_script.sh
@@ -1,5 +1,8 @@
-#树莓派配置脚本
-#用sudo来运行此脚本
+#!/bin/bash
+# 添加sudo运行
+
+# 树莓派配置脚本
+# 用sudo来运行此脚本
 
 cp ./my_supplicant.conf /etc/wpa_supplicant/my_supplicant.conf
 cat ./interfaces > /etc/network/interfaces
@@ -8,7 +11,7 @@ systemctl daemon-reload
 service networking restart
 sudo systemctl restart networking.service
 
-#树莓派源列表 http://www.raspbian.org/RaspbianMirrors
+# 树莓派源列表 http://www.raspbian.org/RaspbianMirrors
 cp ./sources.list /etc/apt/
 
 apt-get update
@@ -21,7 +24,7 @@ apt-get install cmake
 apt-get install ,libswscale-dev
 
 cp /etc/systemd/system/default.target /lib/systemd/system/multi-user.target
-echo "Created symlink from /etc/systemd/system/default.target to /lib/systemd/system/multi-user.target.“
+echo "Created symlink from /etc/systemd/system/default.target to /lib/systemd/system/multi-user.target."
 
 # firmware 安装
 # openmaxil， camkit依赖
@@ -35,10 +38,10 @@ echo "Created symlink from /etc/systemd/system/default.target to /lib/systemd/sy
 # http://tieba.baidu.com/p/3418475592
 
 
-#libavcodec 安装
+# libavcodec 安装
 apt-get install libavcodec-dev
 
-＃在树莓派中编译camkit  ,其中-DPLATFORM=RPI是说明该环境是树莓派环境
+# 在树莓派中编译camkit  ,其中-DPLATFORM=RPI是说明该环境是树莓派环境
 cp camkit
 mkdir build
 cd build
